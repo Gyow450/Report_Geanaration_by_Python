2fb017b7cc021c4879cf82e6cccea0b94895560c
Gyow450 <1752708214@qq.com>
2026-01-04 19:59:06 +0800

    2026年1月4日19:59:02

--- insert_pic.py
+++ insert_pic.py
@@ -8,11 +8,11 @@ from pathlib import Path
 source:Path=Path(r"E:\BaiduSyncdisk\成渝特检\模板文件与生成程序\记录、报告生成\钢管\破损点\破损点台账.xlsx")
 save_path:Path=Path(r"E:\BaiduSyncdisk\成渝特检\模板文件与生成程序\记录、报告生成\钢管\破损点\test.xlsx")
 wb:Workbook = openpyxl.load_workbook(str(source))
-ws:Worksheet = wb.active 
+ws:Worksheet= wb.worksheets[0]
 
 target_rows:list[str] = []
-log_dict = rg.get_col_in_sheet(ws)
-for cell in ws[log_dict['ACVG最大dB值']] :
+log_dict = rg.get_col_in_sheet(ws) 
+for cell in ws[log_dict['ACVG最大dB值']]:
     if cell.value is not None:
         target_rows.append(str(cell.row))
 for row in target_rows[1:]:
@@ -23,7 +23,5 @@ for row in target_rows[1:]:
         # image_name = Path(ws[log_dict['记录自编号']+row].value)
         image_path = '破损点图片'/Path(image_name)
         cell.hyperlink = str(image_path)
-        cell.style = "Hyperlink"
-        
-
+        cell.style = "Hyperlink"         
 wb.save(str(save_path))
